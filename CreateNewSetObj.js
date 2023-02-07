dc6d8d14e07464b1f89f36ec1637fda6bc34160e
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-02-07 17:09:30 +0530

    Create CreateNewSetObj.js

---
+++ CreateNewSetObj.js
@@ -0,0 +1,11 @@
+//2.2. Write the syntax for
+//a) Creating new set object
+//b) Adding new element to set object
+//c) Deleting element from set object
+
+ let set = new Set();  //Creating new set object
+ set.add(5);     //Adding new element to set object
+ set.add(9);   //Adding new element to set object
+set.delete(5); //Deleting element from set object
+
+ console.log(set);
